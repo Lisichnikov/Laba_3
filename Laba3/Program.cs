2e122f2ac446e489a30f1f44b91e9f69c3676669
Алексей Лисичников <alexochanel@gmail.com>
2023-02-01 07:48:35 +0300

    Создаю рандомный массив

--- Laba3/Program.cs
+++ Laba3/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿namespace лаба3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = 10000, max = 0;
+            Random rnd = new Random();
+            int[] array = new int[1000];
+
+        }
+
+    }
+}
